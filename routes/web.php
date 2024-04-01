3a37ba4cd5c5be2d43d83cd8155a6c617b48a800
PitokDf <pitokfauzi@gmail.com>
2024-04-01 13:17:01 +0700

    user

--- routes/web.php
+++ routes/web.php
@@ -25,30 +25,30 @@ Route::group(['middleware' => ['auth', 'userAkses:2,3,4,5']], function () {
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::middleware('userAkses:1')->group(function () {
-        Route::middleware('userAkses:1')->group(function () {
-            Route::get('/', function () {
-                $userCount = Users::count();
-                return view("dashboard.index")->with("count", $userCount);
-            })->name('dashboard');
-        });
-        Route::resource('user', UsersController::class);
-        Route::get('getAllDataUser', [UsersController::class, "getAllData"]);
-        Route::post('importUser', [UsersController::class, "import"]);
-        Route::get('exportUser', [UsersController::class, "export"])->name('user.export');
-        Route::resource("kategori-berita", KategoriBeritaController::class);
-        Route::get('getAllDataKategori', [KategoriBeritaController::class, "getData"]);
-        Route::resource("dosen", DosenController::class);
-        Route::get('getAllDataDosen', [DosenController::class, "getData"]);
-        Route::resource("kategori-barang", KategoriBarangController::class);
-        Route::get("getKategori", [KategoriBarangController::class, "getKategori"]);
-        Route::resource("barang", BarangController::class);
-        Route::get('getAllDataBarang', [BarangController::class, "getData"]);
-        Route::resource("mahasiswa", MahasiswasController::class);
-        Route::get("getAllDataMahasiswa", [MahasiswasController::class, "getData"]);
-        Route::resource("berita", BeritaController::class);
-        Route::get("getBerita", [BeritaController::class, "getData"]);
-    });
+    // Route::middleware('userAkses:1')->group(function () {
+    // Route::middleware('userAkses:1')->group(function () {
+    Route::get('/', function () {
+        $userCount = Users::count();
+        return view("dashboard.index")->with("count", $userCount);
+    })->name('dashboard');
+    // });
+    Route::resource('user', UsersController::class);
+    Route::get('getAllDataUser', [UsersController::class, "getAllData"]);
+    Route::post('importUser', [UsersController::class, "import"]);
+    Route::get('exportUser', [UsersController::class, "export"])->name('user.export');
+    Route::resource("kategori-berita", KategoriBeritaController::class);
+    Route::get('getAllDataKategori', [KategoriBeritaController::class, "getData"]);
+    Route::resource("dosen", DosenController::class);
+    Route::get('getAllDataDosen', [DosenController::class, "getData"]);
+    Route::resource("kategori-barang", KategoriBarangController::class);
+    Route::get("getKategori", [KategoriBarangController::class, "getKategori"]);
+    Route::resource("barang", BarangController::class);
+    Route::get('getAllDataBarang', [BarangController::class, "getData"]);
+    Route::resource("mahasiswa", MahasiswasController::class);
+    Route::get("getAllDataMahasiswa", [MahasiswasController::class, "getData"]);
+    Route::resource("berita", BeritaController::class);
+    Route::get("getBerita", [BeritaController::class, "getData"]);
+    // });
     // Route::group(['middleware' => 'userAkses:1'], function () {
     //     // Route::get('/', function () {
     //     //     $userCount = Users::count();

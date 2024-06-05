ad324954e1ce8887555a08562f87d93584798aa6
Pito Desri Pauzi <pitokfauzi@gmail.com>
2024-06-05 22:38:41 +0700

    rubah sedikit

--- resources/views/reports/pdf/report_peminjaman.blade.php
+++ resources/views/reports/pdf/report_peminjaman.blade.php
@@ -4,19 +4,19 @@
     <table class="table table-bordered mt-5">
         <thead>
             <tr>
-                <th>No</th>
-                <th>Kode Barang</th>
-                <th>Nama Barang</th>
-                <th>Pemasok</th>
-                <th>Tanggal Masuk</th>
-                <th>Jumlah</th>
+                <th style="text-align: center;">kode Peminjaman</th>
+                <th style="text-align: center;">Nama Peminjam</th>
+                <th style="text-align: center;">Kode Barang</th>
+                <th style="text-align: center;">Nama Barang</th>
+                <th style="text-align: center;">Banyak Pinjam</th>
+                <th style="text-align: center;">Status</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($data as $item)
                 <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $item->barang->code_barang ? $item->user->mahasiswa->nama . ' - M' : ($item->user->dosen ? $item->user->dosen->name . ' - D' : $item->user->username . ' - S') }}
+                    <td>{{ $item->kode_peminjaman }}</td>
+                    <td>{{ $item->user->mahasiswa ? $item->user->mahasiswa->nama . ' - M' : ($item->user->dosen ? $item->user->dosen->name . ' - D' : $item->user->username . ' - S') }}
                     </td>
                     <td>{{ $item->barang->code_barang }}</td>
                     <td>{{ $item->barang->nama_barang }}</td>

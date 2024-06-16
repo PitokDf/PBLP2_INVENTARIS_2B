09f8fc18fb527e7fa4de8761c8f3ab42ded6762f
Pito Desri Pauzi <pitokfauzi@gmail.com>
2024-06-16 19:51:07 +0700

    user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,55 +1,76 @@
 @extends('layouts.content')
 
 @section('scriptPages')
-    <script src="{{ asset('js/datatables.js') }}"></script>
-    <script src="{{ asset('js/sweetalert.js') }}"></script>
-@endsection
-@section('title', 'Users')
-@section('modal')
-    @include('user.modal')
-@endsection
-@section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h5 class="m-0 font-weight-bold text-secondary">Data Users</h5>
-                    <div>
-                        <button type="button" class="btn btn-sm btn-light btn-refresh" data-toggle="modal">
-                            <i class="fas fa-sync-alt"></i>
-                        </button>
-                        <a href="" class="btn btn-sm btn-success" data-toggle="modal" id="btnCreate">
-                            <i class="fas fa-solid fa-plus"></i>
-                            Add
-                        </a>
-                        <a href="{{ route('user.export') }}" class="btn btn-sm btn-primary btnExport">
-                            <i class="fas fa-solid fa-file-export"></i>
-                            Export
-                        </a>
-                        <a href="" class="btn btn-sm btn-primary showImport" data-toggle="modal">
-                            <i class="fas fa-solid fa-file-import"></i>
-                            Import
-                        </a>
-                    </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="table table-responsive">
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-striped table-hover" id="tableUsers" width="100%"
-                                cellspacing="0">
-                                <thead style="background-color: #2c3b42; color: #f6f6f6">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nama</th>
-                                        <th>Email</th>
-                                        <th>Status</th>
-                                        <th>Peran</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                {{-- <tfoot style="background-color: rgb(0, 80, 104); color: white">
+    <script src="/js/datatables.js"></script>
+    <script src="/js/sweetalert.js">
+        < />
+    @endsection
+    @section('title', 'Users')
+    @section('modal')
+        @include('user.modal')
+    @endsection
+    @section('content') <
+            div class="row" >
+            <
+            div class="col-12" >
+            <
+            div class="card shadow mb-4" >
+            <
+            !--Card Header - Dropdown-- >
+            <
+            div class="card-header py-3 d-flex flex-row align-items-center justify-content-between" >
+            <
+            h5 class="m-0 font-weight-bold text-secondary" > Data Users < /h5> <
+            div >
+            <
+            button type="button" class="btn btn-sm btn-light btn-refresh" data-toggle="modal" >
+            <
+            i class="fas fa-sync-alt" > < /i> <
+            /button> <
+            a href="" class="btn btn-sm btn-success" data-toggle="modal" id="btnCreate" >
+            <
+            i class="fas fa-solid fa-plus" > < /i>
+        Add
+            <
+            /a> <
+            a href="{{ route('user.export') }}" class="btn btn-sm btn-primary btnExport" >
+            <
+            i class="fas fa-solid fa-file-export" > < /i>
+        Export
+            <
+            /a> <
+            a href="" class="btn btn-sm btn-primary showImport" data-toggle="modal" >
+            <
+            i class="fas fa-solid fa-file-import" > < /i>
+        Import
+            <
+            /a> <
+            /div> <
+            /div> <
+            !--Card Body-- >
+            <
+            div class="card-body" >
+            <
+            div class="table table-responsive" >
+            <
+            div class="table-responsive" >
+            <
+            table class="table table-bordered table-striped table-hover" id="tableUsers" width="100%"
+        cellspacing="0" >
+            <
+            thead style="background-color: #2c3b42; color: #f6f6f6" >
+            <
+            tr >
+            <
+            th > No < /th> <
+            th > Nama < /th> <
+            th > Email < /th> <
+            th > Status < /th> <
+            th > Peran < /th> <
+            th > Aksi < /th> <
+            /tr> <
+            /thead>
+        {{-- <tfoot style="background-color: rgb(0, 80, 104); color: white">
                                     <tr>
                                         <th>No</th>
                                         <th>Nama</th>
@@ -58,11 +79,12 @@
                                         <th>Aksi</th>
                                     </tr>
                                 </tfoot> --}}
-                                <tbody>
-                                    <?php
-                                    // $role = ['Admin', 'Pimpinan', 'Dosen', 'Mahasiswa', 'Staff'];
-                                    ?>
-                                    {{-- @foreach ($data as $item)
+            <
+            tbody >
+            <?php
+            // $role = ['Admin', 'Pimpinan', 'Dosen', 'Mahasiswa', 'Staff'];
+            ?>
+        {{-- @foreach ($data as $item)
                                         <tr>
                                             <th>{{ $loop->iteration }}</th>
                                             <td>{{ $item->name }}</td>
@@ -91,14 +113,16 @@
                                             </td>
                                         </tr>
                                     @endforeach --}}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+            <
+            /tbody> <
+            /table> <
+            /div> <
+            /div> <
+            /div> <
+            /div> <
+            /div>
 
-    </div>
+            <
+            /div>
 
-@endsection
+    @endsection

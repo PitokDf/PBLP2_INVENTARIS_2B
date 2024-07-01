86bf4f09b7fb6fc2c944fdb7aab469af9d8956e8
Pito Desri Pauzi <108637369+PitokDf@users.noreply.github.com>
2024-07-01 14:07:04 +0700

    Update StoreBarangRequest.php

--- app/Http/Requests/StoreBarangRequest.php
+++ app/Http/Requests/StoreBarangRequest.php
@@ -26,7 +26,7 @@ class StoreBarangRequest extends FormRequest
             "nama_barang" => ["required"],
             "kategori" => ["required", "exists:kategori_barang,id"],
             "jumlah" => ["required", "numeric", "min:1"],
-            "foto" => ["required", "max:2000"],
+            "foto" => ["nullable", "max:2000"],
             "posisi" => ["required"],
             'merk' => 'required|exists:merks,id',
             'tanggal_masuk' => 'required|date',

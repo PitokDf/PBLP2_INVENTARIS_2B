d57a75be1eecd64a6176b6d13ff1468219e1b541
Pito Desri Pauzi <pitokfauzi@gmail.com>
2024-07-02 21:01:24 +0700

    optimalisasi link

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,38 +3,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description"
-        content="Sebuah project dari kampus politeknik negeri padang, yaitu project base learning inventaris labor ti">
+        content="Project Base Learning Inventaris Labor TI Politeknik Negeri Padang - Sistem manajemen inventaris laboratorium teknologi informasi yang efisien dan mudah digunakan.">
     <meta name="author" content="Pito Desri Pauzi and Team">
+    <meta name="keywords"
+        content="Project Base Learning, Inventaris, Labor TI, Politeknik Negeri Padang, Sistem Manajemen, Teknologi Informasi">
+    <title>@yield('title') - PBLP2</title>
 
-    <title>@yield('title') - PBL P2</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
-    <!-- Custom fonts for this template-->
-    <link href="/vendors/startbootstrap-sb-admin-2-gh-pages/vendor/fontawesome-free/css/all.min.css" rel="stylesheet"
-        type="text/css">
+    <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
-    {{-- css boostrap --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-
-    <!-- Custom styles for this template-->
-    <link href="/vendors/startbootstrap-sb-admin-2-gh-pages/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="/vendors/css/dataTables.css" rel="stylesheet">
-
-    {{-- css datatales --}}
-    <link rel="stylesheet" href="//cdn.datatables.net/2.0.1/css/dataTables.dataTables.min.css">
-
-    <link href="/vendors/startbootstrap-sb-admin-2-gh-pages/vendor/datatables/dataTables.bootstrap4.min.css"
-        rel="stylesheet">
-
+    <link href="/vendor/startbootstrap/sb-admin-2.min.css" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/iziToast.min.css">
-    <link rel="stylesheet" href="/css/customBoostrap.css">
     <link rel="icon" href="/asset/baru3.png" type="image/*">
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
+    <script type="application/ld+json">
+            {
+                "@context": "http://schema.org",
+                "@type": "Organization",
+                "name": "Project Base Learning Inventaris Labor TI Politeknik Negeri Padang",
+                "url": "https://pblp2inventaris2b-production-6d04.up.railway.app",
+                "logo": "https://pblp2inventaris2b-production-6d04.up.railway.app/asset/baru3.png",
+                "contactPoint": {
+                    "@type": "ContactPoint",
+                    "telephone": "+62-831-8054-1892",
+                    "contactType": "Customer Support"
+                }
+            }
+        </script>
+    <link rel="stylesheet" href="/css/customBoostrap.css">
 </head>
